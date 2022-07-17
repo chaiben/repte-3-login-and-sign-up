a1466700bcffd48ebe7463c2f6573c56b2f16426
Marçal Machado Chaiben <marcal.chaiben@qustodio.com>
2022-07-17 12:06:20 +0200

    Changes to work at github pages - https://create-react-app.dev/docs/deployment/#github-pages

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import { BrowserRouter, Route, Routes} from 'react-router-dom';
+import { HashRouter, Route, Routes} from 'react-router-dom';
 import LogIn from './pages/LogIn';
 import SignUp from './pages/SignUp';
 import './App.css'
 
 const App = () => {
   return(
-<BrowserRouter>
+<HashRouter>
     <Routes>   
         <Route index element={<LogIn />} />
         <Route path='/sign-up' element={<SignUp />}></Route>
-        <Route path="*" element={<div>404</div>} />
+        <Route path="*" element={<LogIn />} />
     </Routes>
-</BrowserRouter>
+</HashRouter>
   )
 }
 
